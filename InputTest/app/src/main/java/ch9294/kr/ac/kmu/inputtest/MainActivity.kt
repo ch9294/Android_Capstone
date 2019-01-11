0797ed61adf159137eb901cfdab9b03765c24a08
ch9294 <ch9294@naver.com>
2019-01-12 00:49:49 +0900

    두번째 커밋

--- InputTest/app/src/main/java/ch9294/kr/ac/kmu/inputtest/MainActivity.kt
+++ InputTest/app/src/main/java/ch9294/kr/ac/kmu/inputtest/MainActivity.kt
@@ -3,6 +3,7 @@ package ch9294.kr.ac.kmu.inputtest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.renderscript.ScriptGroup
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
@@ -28,7 +29,13 @@ class MainActivity : AppCompatActivity() {
                 val bodyBuilder = FormBody.Builder()
 
                 bodyBuilder.add("name", editText.text.toString())
-                val request = url.post(bodyBuilder.build()).build()
+                bodyBuilder.add("address", editText2.text.toString())
+                val body = bodyBuilder.build()
+                val post = url.post(body)
+
+                val request = post.build()
+
+                Log.d("InputTest", "HTTP 연결 실행")
                 client.newCall(request).enqueue(InputCallback())
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -38,14 +45,24 @@ class MainActivity : AppCompatActivity() {
 
     inner class InputCallback : Callback {
         override fun onFailure(call: Call, e: IOException) {
-            Toast.makeText(this@MainActivity, "연결에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            Log.d("InputTest", "연결 실패")
+
+            runOnUiThread {
+                textView.text = """
+                    연결에 실패했습니다.
+                    ${e.printStackTrace()}
+                """.trimIndent()
+            }
+
         }
 
         override fun onResponse(call: Call, response: Response) {
-            Toast.makeText(this@MainActivity, "연결이 성공했습니다.", Toast.LENGTH_SHORT).show()
+            Log.d("InputTest", "연결 성공")
+
 
             runOnUiThread {
-                textView.text ="""
+                Toast.makeText(this@MainActivity, "연결이 성공했습니다.", Toast.LENGTH_SHORT).show()
+                textView.text = """
                     name : ${editText.text}
                     address : ${editText2.text}
                 """.trimIndent()
